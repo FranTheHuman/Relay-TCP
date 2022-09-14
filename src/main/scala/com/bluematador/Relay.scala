6e0d03d5c7639c32a4af1c3f9e11569697d8171a
fperrotta-rb <francisco.perrota@redb.ee>
2022-09-14 12:27:53 -0300

    WIP

--- src/main/scala/com/bluematador/Relay.scala
+++ src/main/scala/com/bluematador/Relay.scala
@@ -1,71 +1,73 @@
 package com.bluematador
 
+import cats.effect.Async
 import cats.effect.std.Console
-import cats.effect.{Async, Concurrent}
 import com.comcast.ip4s.{IpLiteralSyntax, Port}
 import fs2.io.net.Network
 import fs2.text
-import org.typelevel.log4cats.Logger
 
-trait Relay {
+trait Relay[F[_]] {
 
-  def boostrap[F[_] : Async : Concurrent : Network : Logger : Console](port: Int): F[Unit]
+  def relay(port: Int): F[Unit]
 
 }
 
-object Relay extends Relay {
+object Relay {
 
-  override def boostrap[F[_] : Async : Concurrent : Network : Logger : Console](port: Int): F[Unit] =
-    Network[F]
-      .server(port = Port.fromInt(port))
-      .flatMap {
-        s1 => {
-          println(s"Connected to External Client on ${s1.remoteAddress}")
+  class RelayTcp[F[_] : Async : Network : Console] {
 
-          Network[F]
-            .server(port = Some(port"8081"))
-            .map { s2 =>
-              println(s"Established relay address: localhost:8081")
+    def relay(port: Int): F[Unit] =
+      Network[F]
+        .server(port = Port.fromInt(port))
+        .flatMap {
+          s1 => {
+            println(s"Connected to External Client on ${s1.remoteAddress}")
 
-               s2
-                 .reads
-                 .through(text.utf8.decode)
-                 .map(response => {
-                   println(s"Emitting: $response")
-                   response
-                 })
-                 .through(text.utf8.encode)
-                 .through(s1.writes)
-                 .parZip(
-                   s1
-                   .reads
-                   .through(text.utf8.decode)
-                   .map(response => {
-                     println(s"Response: $response")
-                     response
-                   })
-                   .handleErrorWith(t => {
-                     println(t.toString)
-                     fs2.Stream.empty
-                   }).drain
-                 )
-                 .handleErrorWith(t => {
-                   println(t.toString)
-                   fs2.Stream.empty
-                 })
+            Network[F]
+              .server(port = Some(port"8081"))
+              .map { s2 =>
+                println(s"Established relay address: localhost:8081")
+
+                s2
+                  .reads
+                  .through(text.utf8.decode)
+                  .map(response => {
+                    println(s"Emitting: $response")
+                    response
+                  })
+                  .through(text.utf8.encode)
+                  .through(s1.writes)
+                  .parZip(
+                    s1
+                      .reads
+                      .through(text.utf8.decode)
+                      .map(response => {
+                        println(s"Response: $response")
+                        response
+                      })
+                      .handleErrorWith(t => {
+                        println(t.toString)
+                        fs2.Stream.empty
+                      }).drain
+                  )
+                  .handleErrorWith(t => {
+                    println(t.toString)
+                    fs2.Stream.empty
+                  })
 
 
-            }
-            .parJoin(100)
+              }
+              .parJoin(100)
 
+          }
         }
-      }
-      .handleErrorWith(t => {
-        println(t.toString)
-        fs2.Stream.empty
-      })
-      .compile
-      .drain
+        .handleErrorWith(t => {
+          println(t.toString)
+          fs2.Stream.empty
+        })
+        .compile
+        .drain
 
+  }
 
 }
--- src/main/scala/com/bluematador/RelayServer.scala
+++ src/main/scala/com/bluematador/RelayServer.scala
@@ -1,8 +1,10 @@
 package com.bluematador
 
 import cats.effect._
+import com.bluematador.Relay.RelayTcp
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
+
 import scala.util.{Failure, Success, Try}
 
 object RelayServer extends IOApp {
@@ -16,7 +18,7 @@ object RelayServer extends IOApp {
       .fold(IO pure System.err.println(s"Usage: sbt 'run {port number}' ") as ExitCode.Error)(value =>
         Try { value.toInt } match {
           case Failure(exception) => IO pure System.err.println(s"$exception") as ExitCode.Error
-          case Success(port) => Relay.boostrap[IO](port).as(ExitCode.Success)
+          case Success(port) => new RelayTcp[IO].relay(port).as(ExitCode.Success)
         }
       )
 
