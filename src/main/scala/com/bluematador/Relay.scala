72a60d51ba145263340bc14f9d15d1b49b0d672a
fperrotta-rb <francisco.perrota@redb.ee>
2022-09-14 12:50:21 -0300

    WIP

--- src/main/scala/com/bluematador/Relay.scala
+++ src/main/scala/com/bluematador/Relay.scala
@@ -3,8 +3,9 @@ package com.bluematador
 import cats.effect.Async
 import cats.effect.std.Console
 import com.comcast.ip4s.{IpLiteralSyntax, Port}
-import fs2.io.net.Network
+import fs2.io.net.{Network, Socket}
 import fs2.text
+import fs2.Stream
 
 trait Relay[F[_]] {
 
@@ -14,9 +15,49 @@ trait Relay[F[_]] {
 
 object Relay {
 
-  class RelayTcp[F[_] : Async : Network : Console] {
+  class RelayTcp[F[_] : Async : Network : Console] extends Relay[F] {
 
-    def relay(port: Int): F[Unit] =
+    override def relay(port: Int): F[Unit] =
+      (for {
+        socket1 <- Network[F].server(port = Port.fromInt(port))
+        _       <- Stream.eval(Console[F].println(s"Established relay address: localhost:8081"))
+        socket2 <- Network[F].server(port = Some(port"8081"))
+      } yield handleRelay(socket1, socket2)).handleErrorWith(t => {
+        println(t.toString)
+        fs2.Stream.empty
+      }).parJoin(100)
+        .compile
+        .drain
+
+    val handleRelay: (Socket[F], Socket[F]) => Stream[F, (Nothing, Nothing)] =
+      (socket1, socket2) => socket2
+        .reads
+        .through(text.utf8.decode)
+        .map(response => {
+          println(s"Emitting: $response")
+          response
+        })
+        .through(text.utf8.encode)
+        .through(socket1.writes)
+        .parZip(
+          socket1
+            .reads
+            .through(text.utf8.decode)
+            .map(response => {
+              println(s"Response: $response")
+              response
+            })
+            .handleErrorWith(t => {
+              println(t.toString)
+              fs2.Stream.empty
+            }).drain
+        )
+        .handleErrorWith(t => {
+          println(t.toString)
+          fs2.Stream.empty
+        })
+
+    def relay_(port: Int): F[Unit] =
       Network[F]
         .server(port = Port.fromInt(port))
         .flatMap {
